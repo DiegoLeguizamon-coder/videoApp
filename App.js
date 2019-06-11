c762d13f0c9e71c6e2ac07c06c1fe1109e8f4128
diego-leguizamon <diego-leguizamon@accionplus.com>
2019-06-11 18:04:07 -0500

    Primera Vista-Header

--- App.js
+++ App.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Home from './src/screens/containers/home.js';
+import Header from './src/sections/components/header.js';
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Video App</Text>
-    </View>
+    <Home>
+      <Header>
+        <Text>Texto de la derecha</Text>
+      </Header>
+      <Text>Buscador</Text>
+      <Text>Categorias</Text>
+      <Text>Sugerencias</Text>
+    </Home>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/sections/components/header.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import{
+    View,
+    Text,
+    SafeAreaView,
+    StyleSheet,
+    Platform,
+    Image
+} from 'react-native';
+import { getDefaultContainerIdAsync } from 'expo-contacts';
+
+function Header(props){
+    return(
+        <View style={styles.container}>
+            <Image 
+            source={require('../../../assets/Logo-dbd.png')}
+            style={styles.logo}
+            />
+            <View styles={styles.right}>
+                {props.children}
+            </View>
+        </View>
+    )
+}
+
+export default Header;
+
+const styles = StyleSheet.create({
+    logo: {
+        width: 200, 
+        height: 100,
+        resizeMode:'contain',
+
+    },
+    container:{
+        paddingVertical: 20,
+        flexDirection: 'row'
+    },
+    right: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'flex-end'
+         
+    }
+    
+
+})
+
+/*export default StyleSheet.create({
+    droidSafeArea: {
+        flex: 1,
+        backgroundColor:nplBlue,
+        paddingTop: Platform.OS === 'android' ? 25: 0
+    }
+})*/
